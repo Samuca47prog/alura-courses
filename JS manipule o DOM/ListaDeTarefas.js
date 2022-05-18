0ae8d2f4ce26c64ccbbb26c2f4199c367a7ac7eb
Samuca47Prog <samuelsimao47@gmail.com>
2022-05-18 11:39:46 -0300

    final da aula 3

--- JS manipule o DOM/ListaDeTarefas.js
+++ JS manipule o DOM/ListaDeTarefas.js
@@ -1,18 +1,23 @@
 const criarTarefa = (evento) => {
-    evento.preventDefault()
+    evento.preventDefault() //inibe o comportamento padrão do formulário
 
-    const input = document.querySelector('[data-form-input]')
+    const lista = document.querySelector('[data-list]') //captura a lista
+    const input = document.querySelector('[data-form-input]') //captura o campo de entrada do form
     const valor = input.value
 
-    const tarefa = document.querySelector('[data-task]')
-    const conteudo = `<p class="content">${valor}</p>`
-    tarefa.innerHTML = conteudo
+    const tarefa = document.createElement('li') //cria uma li
+    tarefa.classList.add('task') //atribui uma classe a essa li
+
+    const conteudo = `<p class="content">${valor}</p>` //cria um conteudo html
+    tarefa.innerHTML = conteudo // atribui o conteudo de cima dentro da li
+
+    lista.appendChild(tarefa) // adiciona a li criada como filha da lista
 
     input.value = " "   
 }
 
+// realiza a função criarTarefa quando o botão de nova tarefa é clicado
 const novaTarefa = document.querySelector('[data-form-button]')
-
 novaTarefa.addEventListener('click', criarTarefa)
 
 
