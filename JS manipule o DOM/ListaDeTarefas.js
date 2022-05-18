57c74dc09ec43ed8c1ec67cc29d501c4c36fa3cc
Samuca47Prog <samuelsimao47@gmail.com>
2022-05-18 11:30:03 -0300

    mudando o valor da tarefa

--- JS manipule o DOM/ListaDeTarefas.js
+++ JS manipule o DOM/ListaDeTarefas.js
@@ -1,5 +1,18 @@
+const criarTarefa = (evento) => {
+    evento.preventDefault()
+
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
+
+    const tarefa = document.querySelector('[data-task]')
+    const conteudo = `<p class="content">${valor}</p>`
+    tarefa.innerHTML = conteudo
+
+    input.value = " "   
+}
+
 const novaTarefa = document.querySelector('[data-form-button]')
 
-novaTarefa.addEventListener('click', ()=> {
-    console.log('fui clicado')
-})
+novaTarefa.addEventListener('click', criarTarefa)
+
+
